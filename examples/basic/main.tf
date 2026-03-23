3b1b696f03117c3cd29504811a9cdb4295691269
Craig Sloggett <craigsloggett@gmail.com>
2026-03-23 17:11:53 -0400

    chore: Cleanup template artifacts

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 # tflint-ignore: terraform_required_version
-module "my_module" {
-  source = "<namespace>/<module>/<provider>"
+module "consul_enterprise" {
+  source = "craigsloggett/consul-enterprise/aws"
   # version = "x.x.x"
 }
