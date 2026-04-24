3b9491a5c45d31102b73d13299c1bfe0d568f8c8
Craig Sloggett <craigsloggett@gmail.com>
2026-04-23 23:23:15 -0400

    fix: Add minimal example for tflint errors

---
+++ examples/basic/main.tf
@@ -0,0 +1,20 @@
+module "consul" {
+  source = "../.."
+
+  project_name              = "consul"
+  consul_enterprise_license = var.consul_enterprise_license
+  ec2_key_pair_name         = "example"
+
+  ec2_ami = {
+    id   = "ami-0example"
+    name = "ubuntu-example"
+  }
+
+  route53_zone = {
+    zone_id = "Z0000000000000"
+    name    = "example.com"
+  }
+
+  vault_url           = "https://vault.example.com"
+  vault_iam_role_name = "vault-server"
+}
